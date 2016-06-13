72b0bc8dd9ca8d81d992370b952c55006356a492
Arman Ortega <ortegaaa@ph.ibm.com>
2016-06-13 16:08:48 +0800

    Added helper/BlogPreview

---
+++ src/com/blog/helper/BlogPreview.java
@@ -0,0 +1,53 @@
+package com.blog.helper;
+
+import java.util.Date;
+
+public class BlogPreview {
+
+	private Integer blogId;
+	private String title;
+	private Date dateCreated;
+	private String author;
+	private Integer authorId;
+
+	public Integer getBlogId() {
+		return blogId;
+	}
+
+	public void setBlogId(Integer blogId) {
+		this.blogId = blogId;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public Date getDateCreated() {
+		return dateCreated;
+	}
+
+	public void setDateCreated(Date dateCreated) {
+		this.dateCreated = dateCreated;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public Integer getAuthorId() {
+		return authorId;
+	}
+
+	public void setAuthorId(Integer authorId) {
+		this.authorId = authorId;
+	}
+
+}
