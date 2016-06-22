40f7d53881e2ce77189f3791b54e748198d611de
Arman Ortega <ortegaaa@ph.ibm.com>
2016-06-22 15:10:34 +0800

    Added form/RegistrationForm

---
+++ src/com/blog/form/RegistrationForm.java
@@ -0,0 +1,51 @@
+package com.blog.form;
+
+public class RegistrationForm {
+	
+	private String username;
+	private String password;
+	private String confirmPassword;
+	private String email;
+	private String confirmEmail;
+	
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getConfirmPassword() {
+		return confirmPassword;
+	}
+
+	public void setConfirmPassword(String confirmPassword) {
+		this.confirmPassword = confirmPassword;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getConfirmEmail() {
+		return confirmEmail;
+	}
+
+	public void setConfirmEmail(String confirmEmail) {
+		this.confirmEmail = confirmEmail;
+	}
+
+}
