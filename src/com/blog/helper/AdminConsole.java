52b9468fdc6538b359c658f131670b9db2a3e308
Arman Ortega <ortegaaa@ph.ibm.com>
2016-06-23 23:37:20 +0800

    Added helper/AdminConsole

---
+++ src/com/blog/helper/AdminConsole.java
@@ -0,0 +1,19 @@
+package com.blog.helper;
+
+import java.util.List;
+
+import com.blog.entity.User;
+
+public class AdminConsole {
+
+	private List<User> users;
+
+	public List<User> getUsers() {
+		return users;
+	}
+
+	public void setUsers(List<User> users) {
+		this.users = users;
+	}
+
+}
