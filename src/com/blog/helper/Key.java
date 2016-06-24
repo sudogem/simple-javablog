d6263d4677f000ef4918031481914bd63f35f607
Arman Ortega <ortegaaa@ph.ibm.com>
2016-06-24 15:27:09 +0800

    Added helper/Key

---
+++ src/com/blog/helper/Key.java
@@ -0,0 +1,11 @@
+package com.blog.helper;
+
+public class Key {
+	
+	public static final String IS_ADMIN = "is_ad";
+	public static final String USER_ID = "uid";
+	public static final String USER_HANDLE = "u_hdl";
+	public static final String USER_PASSWORD = "u_pwd";
+	public static final String SESSION_COOKIE = "tcs1";
+
+}
